2d17a6a538a8a2b0e83a6a6a212393fc42ee5d41
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-12-06 16:30:54 +0300

    Add a function to fetch data from the Api

--- src/app/user-profile.service.ts
+++ src/app/user-profile.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
-
-  constructor() { }
+private url="https://api.github.com/users";
+  constructor(private http:HttpClient) { }
+  getUserInfo(){
+    this.http.get(this.url)
+  }
 }
